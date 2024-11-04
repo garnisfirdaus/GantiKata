4a8be5d73bfdc599ff39b081e961e77ea7879951
garnisfirdaus <garnisfirdaus@gmail.com>
2024-11-04 13:27:21 +0700

    Add files via upload

---
+++ Gantikata.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg23176016_latihan28.gantikata;
+
+/**
+ *
+ * @author LENOVO
+ * NAMA                 : GARNIS FIRDAUS
+ * NIM                  : 23176016
+ * PRODI                : SISTEM INFORMASI
+ * DESKRIPSI PROGRAM    : MENAMPILKAN GANTI KATA DALAM KALIMAT
+ */
+
+import java.util.Scanner;
+
+public class Gantikata {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("====Program Mengganti Kata====");
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = input.nextLine();
+
+        System.out.print("Ganti Kata : ");
+        String kataGanti = input.nextLine();
+
+        System.out.print("Menjadi Kata : ");
+        String kataBaru = input.nextLine();
+
+        // Mengganti kata yang diminta dengan kata baru
+        String kalimatBaru = kalimat.replace(kataGanti, kataBaru);
+
+        // Menampilkan hasil
+        System.out.println("\n====Hasil Formatting====");
+        System.out.println("Kalimat awal : " + kalimat);
+        System.out.println("Kalimat baru : " + kalimatBaru);
+
+        input.close();
+    }
+}
